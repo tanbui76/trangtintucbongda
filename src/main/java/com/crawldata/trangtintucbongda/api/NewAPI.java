9c6aec897c5d73c69926fdeac0932ac33ed42b34
Bùi Nguyễn Nhật Tân <84611805+tanbui76@users.noreply.github.com>
2023-03-16 10:52:14 +0700

    Tan

--- src/main/java/com/crawldata/trangtintucbongda/api/NewAPI.java
+++ src/main/java/com/crawldata/trangtintucbongda/api/NewAPI.java
@@ -1,12 +1,14 @@
 package com.crawldata.trangtintucbongda.api;
 
+import com.crawldata.trangtintucbongda.entity.TinTucEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NewAPI {
     @GetMapping("/new")
-    public String getNew() {
-        return "Hello word!!";
+    public TinTucEntity getNew() {
+        TinTucEntity tinbongda = new TinTucEntity("Tin tuc bong da","anh 1","bao ngaasdsdsdsds");
+        return  tinbongda;
     }
 }
