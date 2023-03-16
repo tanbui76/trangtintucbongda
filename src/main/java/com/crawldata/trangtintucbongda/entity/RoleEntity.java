70f4ee9eb2dac9691bde8d39593cb6db6e49666b
kieulinhhothi <hothikieulinh1407@gmail.com>
2023-03-16 19:02:48 +0700

    linh_firstcommit

---
+++ src/main/java/com/crawldata/trangtintucbongda/entity/RoleEntity.java
@@ -0,0 +1,35 @@
+package com.crawldata.trangtintucbongda.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Role")
+public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long role_id;
+    @Column(name = "role_name")
+    private String role_name;
+
+    public RoleEntity() {
+    }
+
+    public RoleEntity(String role_name) {
+        this.role_name = role_name;
+    }
+
+    public Long getRole_id() {
+        return role_id;
+    }
+
+
+    public String getRole_name() {
+        return role_name;
+    }
+
+    public void setRole_name(String role_name) {
+        this.role_name = role_name;
+    }
+}
