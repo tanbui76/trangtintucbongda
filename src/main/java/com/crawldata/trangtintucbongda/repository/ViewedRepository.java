a57d0b6ec81e192860ea2728650e5850dd18355e
kieulinhhothi <hothikieulinh1407@gmail.com>
2023-03-30 18:03:30 +0700

    linh

--- src/main/java/com/crawldata/trangtintucbongda/repository/ViewedRepository.java
+++ src/main/java/com/crawldata/trangtintucbongda/repository/ViewedRepository.java
@@ -1,8 +1,17 @@
 package com.crawldata.trangtintucbongda.repository;
 
+import com.crawldata.trangtintucbongda.entity.NewsEntity;
 import com.crawldata.trangtintucbongda.entity.ViewedEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ViewedRepository extends JpaRepository<ViewedEntity, Long> {
+
+
+    @Query("SELECT n FROM ViewedEntity v JOIN v.account a JOIN v.news n WHERE a.account_id = :account_id")
+    List<NewsEntity> getAllNewsViewed (@Param("account_id")Long account_id);
 
 }
--- src/main/java/com/crawldata/trangtintucbongda/service/ViewedService.java
+++ src/main/java/com/crawldata/trangtintucbongda/service/ViewedService.java
@@ -1,9 +1,12 @@
 package com.crawldata.trangtintucbongda.service;
 
+import com.crawldata.trangtintucbongda.entity.NewsEntity;
 import com.crawldata.trangtintucbongda.entity.ViewedEntity;
 
 import java.util.List;
 
 public interface ViewedService {
     List<ViewedEntity> getAllViewed();
+
+    List<NewsEntity> getAllNewsViewed (Long account_id);
 }
