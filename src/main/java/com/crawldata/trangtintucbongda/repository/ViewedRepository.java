eaaaee8cf280134bb71ee96295ad4bf79dc8ea2d
kieulinhhothi <hothikieulinh1407@gmail.com>
2023-03-30 15:02:09 +0700

    linh

--- src/main/java/com/crawldata/trangtintucbongda/repository/ViewedRepository.java
+++ src/main/java/com/crawldata/trangtintucbongda/repository/ViewedRepository.java
@@ -3,5 +3,6 @@ package com.crawldata.trangtintucbongda.repository;
 import com.crawldata.trangtintucbongda.entity.ViewedEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Viewedrepository extends JpaRepository<ViewedEntity, Long> {
+public interface ViewedRepository extends JpaRepository<ViewedEntity, Long> {
+
 }
---
+++ src/main/java/com/crawldata/trangtintucbongda/service/ViewedService.java
@@ -0,0 +1,9 @@
+package com.crawldata.trangtintucbongda.service;
+
+import com.crawldata.trangtintucbongda.entity.ViewedEntity;
+
+import java.util.List;
+
+public interface ViewedService {
+    List<ViewedEntity> getAllViewed();
+}
