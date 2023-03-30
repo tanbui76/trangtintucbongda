665b6daaf55555856af51e6e37589db71ac74e78
Thái Thị Thanh Ngân <94752353+ThanhNgan110@users.noreply.github.com>
2023-03-30 18:01:47 +0700

    ngan

--- src/main/java/com/crawldata/trangtintucbongda/api/NotificationAPI.java
+++ src/main/java/com/crawldata/trangtintucbongda/api/NotificationAPI.java
@@ -3,15 +3,24 @@ package com.crawldata.trangtintucbongda.api;
 import com.crawldata.trangtintucbongda.entity.NotificationEntity;
 import com.crawldata.trangtintucbongda.repository.NotificationRepository;
 import com.crawldata.trangtintucbongda.service.NotificationService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
 @RestController
+@RequestMapping("/notifications")
 public class NotificationAPI {
     private NotificationService notificationService;
-    @GetMapping("/notifications")
+
+    public NotificationAPI(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+    @CrossOrigin
+    @GetMapping("/getAllNotifications")
     public List<NotificationEntity> getAllNotification(){
         return  notificationService.getAllNotification();
     }
--- src/main/java/com/crawldata/trangtintucbongda/service/impl/NotificationImpl.java
+++ src/main/java/com/crawldata/trangtintucbongda/service/impl/NotificationImpl.java
@@ -3,11 +3,17 @@ package com.crawldata.trangtintucbongda.service.impl;
 import com.crawldata.trangtintucbongda.entity.NotificationEntity;
 import com.crawldata.trangtintucbongda.repository.NotificationRepository;
 import com.crawldata.trangtintucbongda.service.NotificationService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class NotificationImpl implements NotificationService {
     private NotificationRepository notificationRepository;
+
+    public NotificationImpl(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
 
     @Override
     public List<NotificationEntity> getAllNotification() {
