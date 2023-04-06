436ca3cedb914be193ed0ca28c3bbc84abb4c549
kieulinhhothi <hothikieulinh1407@gmail.com>
2023-04-06 14:32:38 +0700

    linh

--- src/main/java/com/crawldata/trangtintucbongda/service/impl/ViewedImpl.java
+++ src/main/java/com/crawldata/trangtintucbongda/service/impl/ViewedImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class ViewedImpl implements ViewedService {
+
     private ViewedRepository  viewedRepository;
 
 
