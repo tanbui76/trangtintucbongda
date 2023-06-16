3e921d09a7029f0729ddd41fa6d8cb247fbd1f9a
Bùi Nguyễn Nhật Tân <buinguyennhattan12122002@gmail.com>
2023-06-16 17:27:32 +0000

    Fix bao tri tu phia server

--- src/main/java/com/crawldata/trangtintucbongda/entity/NewsEntity.java
+++ src/main/java/com/crawldata/trangtintucbongda/entity/NewsEntity.java
@@ -31,11 +31,9 @@ public class NewsEntity {
     private String news_title;
     @Column(name = "news_images", columnDefinition = "nvarchar(max)")
     private String news_images;
-    @Column(name = "news_submitdate")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date news_submitdate;
+    @Column(name = "news_submitdate", columnDefinition = "nvarchar(max)")
+    private String news_submitdate;
     @Column(name = "news_describe", columnDefinition = "nvarchar(max)")
-
     private String news_describe;
     @Column(name = "news_author", columnDefinition = "nvarchar(max)")
     private String news_author;
@@ -46,7 +44,8 @@ public class NewsEntity {
     public NewsEntity() {
     }
 
-    public NewsEntity(Set<LikesEntity> likes, CategoryEntity category, Set<CommentEntity> comments, String news_title, String news_images, Date news_submitdate, String news_describe, String news_author,String news_detailContent) {
+    public NewsEntity(Long news_id, Set<LikesEntity> likes, CategoryEntity category, Set<CommentEntity> comments, String news_title, String news_images, String news_submitdate, String news_describe, String news_author, String news_detailContent) {
+        this.news_id = news_id;
         this.likes = likes;
         this.category = category;
         this.comments = comments;
@@ -56,81 +55,45 @@ public class NewsEntity {
         this.news_describe = news_describe;
         this.news_author = news_author;
         this.news_detailContent = news_detailContent;
-    }
-
-    public Set<LikesEntity> getLikes() {
-        return likes;
-    }
-
-    public void setLikes(Set<LikesEntity> likes) {
-        this.likes = likes;
-    }
-
-    public String getNews_title() {
-        return news_title;
-    }
-
-    public void setNews_title(String news_title) {
-        this.news_title = news_title;
-    }
-
-    public String getNews_images() {
-        return news_images;
-    }
-
-    public void setNews_images(String news_images) {
-        this.news_images = news_images;
-    }
-
-    public Date getNews_submitdate() {
-        return news_submitdate;
-    }
-
-    public void setNews_submitdate(Date news_submitdate) {
-        this.news_submitdate = news_submitdate;
-    }
-
-    public String getNews_describe() {
-        return news_describe;
-    }
-
-    public void setNews_describe(String news_describe) {
-        this.news_describe = news_describe;
-    }
-
-    public String getNews_author() {
-        return news_author;
-    }
-
-    public void setNews_author(String news_author) {
-        this.news_author = news_author;
     }
 
     public Long getNews_id() {
         return news_id;
     }
 
-    public CategoryEntity getCategory() {
-        return category;
+    public Set<LikesEntity> getLikes() {
+        return likes;
     }
 
-    public void setCategory(CategoryEntity category) {
-        this.category = category;
+    public CategoryEntity getCategory() {
+        return category;
     }
 
     public Set<CommentEntity> getComments() {
         return comments;
     }
 
-    public void setComments(Set<CommentEntity> comments) {
-        this.comments = comments;
+    public String getNews_title() {
+        return news_title;
+    }
+
+    public String getNews_images() {
+        return news_images;
+    }
+
+    public String getNews_submitdate() {
+        return news_submitdate;
+    }
+
+    public String getNews_describe() {
+        return news_describe;
+    }
+
+    public String getNews_author() {
+        return news_author;
     }
 
     public String getNews_detailContent() {
         return news_detailContent;
-    }
-
-    public void setNews_detailContent(String news_detailContent) {
-        this.news_detailContent = news_detailContent;
     }
 }
